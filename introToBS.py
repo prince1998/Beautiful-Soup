816e0ed2625af780705abb3af71f126a1bece1c3
prince1998 <priyanshramnani21@gmail.com>
2019-03-15 22:15:19 +0530

    Intro to Web Scraping
    Python Program to scrape Title of my Website using Python and Beautiful Soup

---
+++ introToBS.py
@@ -0,0 +1,16 @@
+#Program to scrape title of my website.
+
+import bs4
+import requests
+
+res = requests.get('https://prince1998.github.io'); #makes a request to webpage
+#res.text will display source code of webpage
+
+#type(res) displays res type as request 
+
+soup = bs4.BeautifulSoup(res.text,'lxml') #converting res from object of type request to object of type BeautifulSoup and structuring it by lxml parser
+
+#type(soup) displays soup type as BeautifulSoup
+
+title = soup.select('title') 
+print(title[0].getText())
